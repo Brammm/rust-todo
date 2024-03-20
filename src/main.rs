495bbc2d409b6d11ecd0b953be82cae94fef4e0c
Bram Van der Sype <bram.vandersype@gmail.com>
2024-03-20 21:34:04 +0100

    Initial commit with two endpoints

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+use serde::Deserialize;
+
+struct AppState {
+    todos: Mutex<Vec<String>>,
+}
+
+#[derive(Deserialize)]
+struct Create {
+    name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    let todos = data.todos.lock().unwrap();
+
+    HttpResponse::Ok().body(todos.join("-"))
+}
+
+#[post("/todo")]
+async fn create(form: web::Form<Create>, data: web::Data<AppState>) -> impl Responder {
+    let form = form.into_inner();
+    let mut todos = data.todos.lock().unwrap();
+
+    todos.push(form.name);
+
+    web::Redirect::to("/")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let state = web::Data::new(AppState {
+        todos: Mutex::new(vec![String::from("Learn Rust"), String::from("Create small Todo app")]),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .service(index)
+            .service(create)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
